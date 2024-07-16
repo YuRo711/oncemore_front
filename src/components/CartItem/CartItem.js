6e20cd40eeb0bbd38330b5b44b0dfabd9c52b668
YuRo711 <rohlinay@mail.ru>
2024-07-16 20:37:41 +0300

    cart items

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -6,12 +6,39 @@ export default function CartItem(props) {
 
   return (
     <NavLink className="cart-item" to={`/item?id=${id}`}>
-      <img className="cart-item__image"
-        src={images[0]}
-        alt={name}
-      />
-      <div className="cart-item__info">
-        <h3 className="cart-item__title">{name}</h3>
+      <div className="cart-item__main">
+        <img className="cart-item__image"
+          src={images[0]}
+          alt={name}
+        />
+        <div className="cart-item__info">
+          <h3 className="cart-item__title">{name}</h3>
+          <p className="cart-item__property">Цвет: {color}</p>
+        </div>
+        <h4 className="cart-item__price">{price}₽</h4>
+      </div>
+      <div className="cart-item__buttons">
+        <button className="cart-item__text-button"
+          type="button"
+        >
+          Удалить
+        </button>
+        <button className="cart-item__text-button"
+          type="button"
+        >
+          Сохранить
+        </button>
+        <div className="cart-item__number">
+          <button 
+            className="cart-item__num-button cart-item__num-button_minus"
+            type="button"
+          />
+          1
+          <button 
+            className="cart-item__num-button cart-item__num-button_plus"
+            type="button"
+          />
+        </div>
       </div>
     </NavLink>
   );
