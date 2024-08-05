85f211946eb72b099690784571cd051839190d9e
YuRo711 <rohlinay@mail.ru>
2024-08-05 09:28:11 +0300

    admin link block

--- src/components/DropdownMenu/DropdownMenu.js
+++ src/components/DropdownMenu/DropdownMenu.js
@@ -1,7 +1,10 @@
+import { useContext } from "react";
 import "./DropdownMenu.css";
 import { NavLink } from "react-router-dom";
+import { UserContext } from "../../contexts/UserContext";
 
 export default function DropdownMenu(props) {
+  const user = useContext(UserContext).user;
 
   return (
     <div className="dropdown">
@@ -10,12 +13,15 @@ export default function DropdownMenu(props) {
       }`}>
       {
         props.links.map((link, i) => 
-          <NavLink className="dropdown__link"
-            key={`dropdown-category-$${i}`}
-            to={link.link}
-          >
-            {link.name}
-          </NavLink>
+          {
+            return link.link != "/admin" || user.privilege >= 1 ?
+            <NavLink className="dropdown__link"
+              key={`dropdown-category-$${i}`}
+              to={link.link}
+            >
+              {link.name}
+            </NavLink> : ""
+          }
         )
       }
       </div>
